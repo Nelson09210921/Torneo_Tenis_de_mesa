5eca14fc2b96c371646ca5f1589ce0eecae0bebc
Nelson09210921 <75802180+Nelson09210921@users.noreply.github.com>
2023-11-14 08:48:36 -0500

    Documentation MainActivity.java

--- app/src/main/java/com/example/torneo_tenis_de_mesa/MainActivity.java
+++ app/src/main/java/com/example/torneo_tenis_de_mesa/MainActivity.java
@@ -7,6 +7,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+
+/*
+@Author Nelson Barboza && Valentina Galenao
+@Version Pre-alpha 1.0.0
+
+Plataforma integral diseñada para gestionar eficientemente torneos de tenis de mesa que deseen ser realizados en la Universidad. 
+Su objetivo principal es brindar a los usuarios una experiencia organizada y fluida al planificar y dar seguimiento al torneo. 
+
+La interfaz de usuario será intuitiva y fácil de navegar, asegurando que todas las funciones estén disponibles de manera clara y accesible.
+
+Existen dos tipos de usuario que pueden registrarse en la aplicación, el administrador, 
+que es la persona quién lidera y organiza el evento y los participantes (jugadores) que pueden ingresar a visualizar o inscribirse al torneo de tenis de mesa.
+
+*/
+
 public class MainActivity extends AppCompatActivity {
 
     //Creación de la activity
